38864466faf302ca681c34b257ad079e2c0d4747
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-06-22 15:05:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1 - Nombre pairs
+
+// for(let i = 11; i <= 47; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 2 - Nombre impairs
+
+// for(let i = 108; i < 588; i++){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+
